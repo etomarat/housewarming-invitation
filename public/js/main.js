ec01a1e3edbec8f14c1f70497880c346d80924e0
etomarat <etomarat@gmail.com>
2017-11-17 18:21:36 +0300

    vk auth

--- public/js/main.js
+++ public/js/main.js
@@ -2,4 +2,10 @@ $(function() {
   $('.invitation').click(function(){
     $(this).addClass('active');
   });
+
+  $('.login').click(function(){
+    VK.Auth.login(function(response){
+      console.log(response);
+    });
+  })
 });
